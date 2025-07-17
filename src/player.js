d12b4cff4ab542388de648b74a16d21e2719258b
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-17 19:11:02 +0800

    Import pubsub module

--- src/player.js
+++ src/player.js
@@ -1,24 +1,10 @@
 import { Gameboard, Cell } from './game.js'
+import { Events } from "./pubsub.js";
 
 export class Player {
     constructor(name = 'player') {
         this.name = name
         this.board = new Gameboard()
         this.board.generateShips()
-
-        // this.enemyBoard = []
-        // for (let i = 0; i < 10; i++) {
-        // this.enemyBoard[i] = [];
-        // for (let j = 0; j < 10; j++) {
-        //     this.enemyBoard[i].push(new Cell('_'));
-        // }}
     }
-
-    // printEnemyBoard = () => {
-    //     this.enemyBoard.forEach(row => {
-    //         let output = ''
-    //         row.forEach(cell => output += cell.value)
-    //         console.log(output)
-    //     })
-    // }
 }
