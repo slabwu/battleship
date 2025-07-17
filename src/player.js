a257445784b8cb0504a2ddb7d8f7e754f9fc209a
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-17 19:08:30 +0800

    Remove enemyBoard in Player

--- src/player.js
+++ src/player.js
@@ -6,19 +6,19 @@ export class Player {
         this.board = new Gameboard()
         this.board.generateShips()
 
-        this.enemyBoard = []
-        for (let i = 0; i < 10; i++) {
-        this.enemyBoard[i] = [];
-        for (let j = 0; j < 10; j++) {
-            this.enemyBoard[i].push(new Cell('_'));
-        }}
+        // this.enemyBoard = []
+        // for (let i = 0; i < 10; i++) {
+        // this.enemyBoard[i] = [];
+        // for (let j = 0; j < 10; j++) {
+        //     this.enemyBoard[i].push(new Cell('_'));
+        // }}
     }
 
-    printEnemyBoard = () => {
-        this.enemyBoard.forEach(row => {
-            let output = ''
-            row.forEach(cell => output += cell.value)
-            console.log(output)
-        })
-    }
+    // printEnemyBoard = () => {
+    //     this.enemyBoard.forEach(row => {
+    //         let output = ''
+    //         row.forEach(cell => output += cell.value)
+    //         console.log(output)
+    //     })
+    // }
 }
--- src/render.js
+++ src/render.js
@@ -1,14 +1,18 @@
 const $ = (id) => document.getElementById(id)
 
-export function render(board) {
+export function render(board, hidden = false) {
     let output = ''
     console.log(board)
     board.forEach(row => {
         let rowOutput = '<div>'
         row.forEach(cell => {
             let content = ''
-            if (Number.isInteger(cell.value)) content = cell.value
-            rowOutput += `<div>${content}</div>`
+            let colour = 'grey'
+            if (Number.isInteger(cell.value) && !hidden) {
+                content = cell.value
+                colour = 'blue'
+            }
+            rowOutput += `<div style="background-color:${colour};">${content}</div>`
         })
         rowOutput += '</div>'
         output += rowOutput
