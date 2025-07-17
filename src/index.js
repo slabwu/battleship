09e499c19832c5755cc0ee6610ae533ef0c3b587
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-17 19:58:46 +0800

    Attack cells from DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import './styles.css'
 import { Player } from './player.js'
-import { render } from './render.js'
+import { render, renderAsHidden } from './render.js'
+import { Events } from "./pubsub.js";
 
 console.log('Welcome to Battleship')
 let user = new Player('Player 1')
 let robot = new Player('Player 2')
+let turn = 'player'
+
 user.board.printBoard()
 render(user.board.cells)
-render(robot.board.cells, true)
+renderAsHidden(robot.board.cells)
 render(robot.board.cells)
 
-robot.board.receiveAttack(1, 1)
-robot.board.receiveAttack(1, 2)
-robot.board.receiveAttack(1, 3)
-console.log(robot.board.attackedCells)
+
+
+// robot.board.receiveAttack(1, 2)
+// robot.board.receiveAttack(1, 3)
+// console.log(robot.board.attackedCells)
+
+export function attackEnemy(e) {
+    let { column, row, x = +column, y = +row} = e.srcElement.dataset
+    let cell = x + y * 9
+    let div = e.srcElement
+    if (turn === 'player' && !robot.board.attackedCells.has(cell)) {
+        console.log(e.srcElement)
+        robot.board.receiveAttack(x, y)
+        div.innerText = (robot.board.isShip(x, y))? '✕' : '·'
+        div.classList.add('clicked')
+        // turn = 'robot'
+    }
+}
--- src/render.js
+++ src/render.js
@@ -1,14 +1,14 @@
+import { attackEnemy } from './index.js'
 const $ = (id) => document.getElementById(id)
 
-export function render(board, hidden = false) {
+export function render(board) {
     let output = ''
-    console.log(board)
     board.forEach(row => {
         let rowOutput = '<div>'
         row.forEach(cell => {
             let content = ''
             let colour = 'grey'
-            if (Number.isInteger(cell.value) && !hidden) {
+            if (Number.isInteger(cell.value)) {
                 content = cell.value
                 colour = 'blue'
             }
@@ -18,8 +18,28 @@ export function render(board, hidden = false) {
         output += rowOutput
     })
 
-    const gameBoard = document.createElement("div")
+    const gameBoard = document.createElement('div')
     gameBoard.classList.add('board')
     gameBoard.innerHTML = output
     $('game').appendChild(gameBoard)
+}
+
+export function renderAsHidden(board) {
+    const gameBoard = document.createElement('div')
+    gameBoard.classList.add('board')
+    $('game').appendChild(gameBoard)
+
+    board.forEach((row, rowIndex) => {
+        let rowDiv = document.createElement('div')
+        gameBoard.appendChild(rowDiv)
+        row.forEach((cell, columnIndex) => {
+            let cellDiv = document.createElement('div')
+            cellDiv.classList.add('clickableCell')
+            cellDiv.dataset.y = rowIndex
+            cellDiv.dataset.x = columnIndex
+            rowDiv.appendChild(cellDiv)
+
+            cellDiv.addEventListener('click', attackEnemy)
+        })
+    })
 }
