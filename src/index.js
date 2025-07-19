238148e02fba312f965ed5bde17332f6541a5c5c
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-19 14:14:15 +0800

    Create naive targeting AI

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ console.log('Welcome to Battleship')
 let user = new Player('player')
 let robot = new Player('enemy', true)
 let turn = 'player'
-let robotMemory = []
+let memory = false
+let go = {right: [+1, 0], left: [-1, 0], up: [0, -1], down: [0, +1]}
 
 user.board.printBoard()
 render(user.board.cells)
@@ -29,18 +30,63 @@ export function attackEnemy(e) {
 }
 
 function letEnemyAttack() {
-    let cell
-    while (true) {
-        cell = Math.floor(Math.random() * 100)
-        if (!user.board.attackedCells.has(cell)) break
+    let x
+    let y
+
+    if (memory) {
+        letEnemyTargetShip()
+        return
     }
-    
+    console.log('aiming yolo', memory)
+    while (true) {
+            x = getRandomInt(10)
+            y = getRandomInt(10)
+            if (!user.board.attackedCells.has(x + y * 10)) break
+    }
+
     setTimeout(() => {
-        let shipHit = robot.attack(user, cell % 10, Math.floor(cell / 10))
+        let shipHit = robot.attack(user, x, y)
         if (shipHit) {
-            letEnemyAttack()
+            memory = {x: x, y: y, ship: user.board.getShip(x, y), queue: [
+                [go.right, 1], [go.left, 1], [go.up, 1], [go.down, 1]
+            ]}
+            if (memory.ship.isSunk()) {
+                memory = false
+                letEnemyAttack()
+            } else {
+                letEnemyTargetShip()
+            }
             return
         }
         turn = 'player'
     }, Math.random() * 500 + 500)
 }
+
+function letEnemyTargetShip() {
+    let direction, x, y
+    while(true) {
+        direction = memory.queue.shift()
+        x = memory.x + direction[0][0] * direction[1]
+        y = memory.y + direction[0][1] * direction[1]
+        if (user.board.isOnBoard(x, y) && !user.board.attackedCells.has(x + y * 10)) {
+            break;
+        }
+    }
+
+    setTimeout(() => {
+        let shipHit = robot.attack(user, x, y)
+        if (shipHit) {
+            memory.queue.push([direction[0], direction[1] + 1])
+            if (memory.ship.isSunk()) {
+                memory = false
+                letEnemyAttack()
+            } else {
+                letEnemyTargetShip()
+            }
+            return
+        }
+        turn = 'player'
+    }, Math.random() * 500 + 500)
+}
+
+let getRandomInt = (max) => Math.floor(Math.random() * max)
